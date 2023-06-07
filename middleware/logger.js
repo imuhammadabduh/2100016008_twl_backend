a834efe3f9aa890952f7338e0c5ae3c5c6c91776
imuhammadabduh <imuhammadabduh@gmail.com>
2023-06-07 13:57:33 +0700

    see logger

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,7 +1,6 @@
 function logger(req, res, next) {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next();
-  }
-  
-  module.exports = logger;
-  
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+}
+
+module.exports = logger;
